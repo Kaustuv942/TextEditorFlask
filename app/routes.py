7866994c46cb94b3e158df371b748b31b1cee4b4
Kaustuv942 <kaustuv942@gmail.com>
2020-04-03 17:11:10 +0530

    UI improvement:

--- app/routes.py
+++ app/routes.py
@@ -251,6 +251,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
+        
 
     return render_template('login.html', title='Sign In', form=form)
 
